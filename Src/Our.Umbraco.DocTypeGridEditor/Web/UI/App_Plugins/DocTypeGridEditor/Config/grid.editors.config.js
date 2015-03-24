311e6fc6b5386e49c36c57792f7fd0d7bddb1743
mattbrailsford <me@mattbrailsford.com>
2015-03-24 11:22:03 +0000

    Added support for previewCssFilePath and previewJsFilePath to allow injecting CSS / JS for previews

--- Src/Our.Umbraco.DocTypeGridEditor/Web/UI/App_Plugins/DocTypeGridEditor/Config/grid.editors.config.js
+++ Src/Our.Umbraco.DocTypeGridEditor/Web/UI/App_Plugins/DocTypeGridEditor/Config/grid.editors.config.js
@@ -8,6 +8,8 @@
 	    "allowedDocTypes": [],
         "enablePreview": true,
         "viewPath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/",
-        "previewViewPath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/Previews/"
+        "previewViewPath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/Previews/",
+        "previewCssFilePath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/Previews/Css/dtge.css",
+        "previewJsFilePath": "/Views/Partials/Grid/Editors/DocTypeGridEditor/Previews/Js/dtge.js"
 	}
 }
