b3be31b7c45d1ca42d087e74368a35763a0c2039
leekelleher <leekelleher@gmail.com>
2016-09-23 16:08:06 +0100

    Added AssemblyInfo
    Strange that it has been missing from the project all this time!

---
+++ src/Our.Umbraco.DocTypeGridEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Our.Umbraco.DocTypeGridEditor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Umbraco Community")]
+[assembly: AssemblyProduct("Our.Umbraco.DocTypeGridEditor")]
+[assembly: AssemblyCopyright("Copyright \xa9 The Umbraco Community 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("45E117EA-CE6D-46E4-8DC5-D7CC24829C75")]
