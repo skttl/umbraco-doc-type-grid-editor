79550654e247b10a2f65090b8f4cf30cf6ec80b8
leekelleher <leekelleher@gmail.com>
2017-11-13 18:27:48 +0000

    Adds support for VS2017/C#7

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -4,4 +4,5 @@ ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 
 CALL src\.nuget\NuGet.exe restore src\Our.Umbraco.DocTypeGridEditor.sln
-CALL "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build\package.proj
+CALL "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MsBuild.exe" build\package.proj
+REM CALL "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build\package.proj
