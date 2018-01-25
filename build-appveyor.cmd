d3422c97e622b6a2d380bf3be6a25ac5aa24c2a0
leekelleher <leekelleher@gmail.com>
2018-01-25 15:58:04 +0000

    Updated NuGet.exe to the latest version (v4.4.x)
    It appears that AppVeyor CI builds were failing due to our older version of NuGet.exe

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -3,6 +3,6 @@ ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 
-CALL src\.nuget\NuGet.exe restore src\Our.Umbraco.DocTypeGridEditor.sln
+CALL build\tools\NuGet.exe restore src\Our.Umbraco.DocTypeGridEditor.sln
 CALL "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MsBuild.exe" build\package.proj
-REM CALL "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build\package.proj
+REM CALL "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\MsBuild.exe" build\package.proj
