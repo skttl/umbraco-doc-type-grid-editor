c41fe86ae1cab602df1a92c0a1448d738be545cc
leekelleher <leekelleher@gmail.com>
2018-02-21 10:53:38 +0000

    Refactored the "compile" directive, reduced code
    re: https://stackoverflow.com/a/29994559/12787

--- src/Our.Umbraco.DocTypeGridEditor/Web/UI/App_Plugins/DocTypeGridEditor/Js/doctypegrideditor.directives.js
+++ src/Our.Umbraco.DocTypeGridEditor/Web/UI/App_Plugins/DocTypeGridEditor/Js/doctypegrideditor.directives.js
@@ -1,25 +1,13 @@
-angular.module("umbraco.directives").directive("dtgeBindHtmlCompile", [
-
-    "$compile",
-
-    function ($compile) {
-
-        var directive = {
-            restrict: "A",
-            scope: {
-                dtgeBindHtmlCompile: "="
-            },
-            link: function ($scope, $element, $attrs) {
-                $scope.$watch(function () { return $scope.dtgeBindHtmlCompile }, function (newValue, oldValue) {
-                    $element.html(newValue);
-
-                    // Only compile child nodes to avoid starting an infinite loop
-                    var childNodes = $element.contents();
-                    $compile(childNodes)($scope);
-                });
-            }
-        };
-
-        return directive;
-    }
-]);
+angular.module("umbraco.directives").directive("dtgeBindHtmlCompile", ["$compile", function ($compile) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            scope.$watch(function () {
+                return scope.$eval(attrs.dtgeBindHtmlCompile);
+            }, function (value) {
+                element.html(value);
+                $compile(element.contents())(scope);
+            });
+        }
+    };
+}]);
