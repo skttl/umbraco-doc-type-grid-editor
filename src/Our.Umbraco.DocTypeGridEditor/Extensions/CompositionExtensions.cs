59274afe300fcb829193f147d6a11541daf68d06
Markus Johansson <markus@obviuse.se>
2020-01-17 12:02:17 +0100

    #173 Added ValueProcessors and added processor for Umbraco.Tags

---
+++ src/Our.Umbraco.DocTypeGridEditor/Extensions/CompositionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Our.Umbraco.DocTypeGridEditor.ValueProcessing.Collections;
+using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.DocTypeGridEditor.Extensions
+{
+    public static class CompositionExtensions
+    {
+        /// <summary>
+        /// Used to modify the collection of Value Processors for Doc Type Grid Editor
+        /// </summary>
+        /// <param name="composition"></param>
+        /// <returns></returns>
+        public static DocTypeGridEditorValueProcessorsCollectionBuilder DocTypeGridEditorValueProcessors(this Composition composition)
+            => composition.WithCollectionBuilder<DocTypeGridEditorValueProcessorsCollectionBuilder>();
+    }
+}
